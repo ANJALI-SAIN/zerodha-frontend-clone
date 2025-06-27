dead0af13313af81432ed5b2528bf7fa26859131
ANJALI-SAIN <anjalipersonal19@gmail.com>
2025-06-27 20:45:17 +0530

    Add files via upload

---
+++ PricingPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from '../Navbar';
+import Footer from '../Footer';
+import Hero from './Hero';
+import Brojkerage from './Brojkerage';
+
+
+function PricingPage() {
+    return ( 
+        <>
+       <Hero />
+       <Brojkerage />
+       
+       
+        </>
+     );
+}
+
+export default PricingPage;
