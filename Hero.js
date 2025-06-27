ced7202e1c1198aeec840edf19192948e52a529c
ANJALI-SAIN <anjalipersonal19@gmail.com>
2025-06-27 20:45:45 +0530

    Add files via upload

--- Hero.js
+++ Hero.js
@@ -1,45 +1,16 @@
-import React from "react";
+import React from 'react';
+
 
 function Hero() {
-  return (
-    <div className="container">
-      <div className="row text-center mt-5 p-5">
-        <h1>Charges</h1>
-        <p className="text-muted">List of all charges and taxes</p>
-        <div className="row mt-5 text-muted">
-          
-          <div className="col p-5" >
-            <img src="media/pricing-eq(1).svg" className="mb-3" alt="Equity"></img>
-            <h1>Free equity delivery</h1>
-            <p>
-              All equity delivery investments (NSE, BSE), are absolutely free —
-              ₹ 0 brokerage.
-            </p>
-          </div>
-         
-          <div className="col p-5" >
-            <img src="media/pricing(20).svg" className="mb-3" alt="F&O" ></img>
-            <h1>Intraday and F&O trades</h1>
-            <p>
-              Flat ₹ 20 or 0.03% (whichever is lower) per executed order on
-              intraday trades across equity, currency, and commodity trades.
-              Flat ₹20 on all option trades.
-            </p>
-          </div>
-          
-          <div className="col p-5" >
-            <img src="media/pricing-eq(1).svg" className="mb-3" alt="Mutual Fund"></img>
-            <h1>Free direct MF</h1>
-            <p>
-              All direct mutual fund investments are absolutely free — ₹ 0
-              commissions & DP charges.
-            </p>
-          </div>
-         
+    return ( 
+       <div className='container mt-5 p-5'>
+        <div className='row text-center  text-muted mt-5 '>
+            <h1 className='mt-4 fs-4'>Zerodha Products</h1><br/>
+            <h3>Sleek, modern, and intuitive trading platforms</h3><br/>
+            <h4 className='mt-3'>Check out our <a href='' style={{textDecoration:"none"}}>investment offerings →</a></h4><br/>
         </div>
-      </div>
-    </div>
-  );
+       </div>
+     );
 }
 
 export default Hero;
