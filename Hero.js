8964864ead445871430463de7593888cd428a7fd
ANJALI-SAIN <anjalipersonal19@gmail.com>
2025-06-27 20:44:50 +0530

    Add files via upload

--- Hero.js
+++ Hero.js
@@ -1,18 +1,45 @@
-import React from 'react';
- 
+import React from "react";
 
 function Hero() {
-    return ( 
-       <div className='container p-5 mb-5'>
-        <div className='row text-center'>
-            <img src='media/homeHero.png' alt='Hero Image' className='mb-5' />
-            <h1 className='mt-5'>Invest in Everythig</h1>
-            <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-            <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%", margin: "0 auto" }}> Sign-up Now</button>
+  return (
+    <div className="container">
+      <div className="row text-center mt-5 p-5">
+        <h1>Charges</h1>
+        <p className="text-muted">List of all charges and taxes</p>
+        <div className="row mt-5 text-muted">
+          
+          <div className="col p-5" >
+            <img src="media/pricing-eq(1).svg" className="mb-3" alt="Equity"></img>
+            <h1>Free equity delivery</h1>
+            <p>
+              All equity delivery investments (NSE, BSE), are absolutely free —
+              ₹ 0 brokerage.
+            </p>
+          </div>
+         
+          <div className="col p-5" >
+            <img src="media/pricing(20).svg" className="mb-3" alt="F&O" ></img>
+            <h1>Intraday and F&O trades</h1>
+            <p>
+              Flat ₹ 20 or 0.03% (whichever is lower) per executed order on
+              intraday trades across equity, currency, and commodity trades.
+              Flat ₹20 on all option trades.
+            </p>
+          </div>
+          
+          <div className="col p-5" >
+            <img src="media/pricing-eq(1).svg" className="mb-3" alt="Mutual Fund"></img>
+            <h1>Free direct MF</h1>
+            <p>
+              All direct mutual fund investments are absolutely free — ₹ 0
+              commissions & DP charges.
+            </p>
+          </div>
+         
         </div>
-
-       </div>
-     );
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
